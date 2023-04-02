b29fd94df44e410ad8f38f8e8b6266f65e01fe79
Andrey Fedoseev <a.fedoseev@sftpro.ru>
2023-04-02 03:47:01 +0300

    add thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from "redux-thunk"
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -30,8 +31,7 @@ const stringMiddleware = () => (next) => (action) => {
 
 const store = createStore( 
     combineReducers({heroes, filters}),
-    // compose(enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    compose(applyMiddleware(stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 )
     
 
